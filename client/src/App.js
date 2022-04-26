2b460a69a45a365aecc8e9ee9a783f0fa536debf
Justin Swift <jlswift2@gmail.com>
2022-04-26 13:01:41 -0500

    Add Outlet for child routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
-import Login from "./Pages/Login";
+import { Outlet } from "react-router-dom"
 
 
 function App() {
 
   return (
-    <Login />
+    <Outlet />
   );
 }
 
